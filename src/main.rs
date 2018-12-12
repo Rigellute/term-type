80b3f6d7b9ea3caa3ca26b6f80f734d74716cde5
Rigellute <rigellute@gmail.com>
2018-12-12 09:47:48 +0000

    Add better sentence generation, calc wpm

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,34 @@ use rand::prelude::*;
 use std::io::{stdin, stdout, Write};
 use std::time::SystemTime;
 
-fn main() {
-    const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
     abcdefghijklmnopqrstuvwxyz\
     0123456789)(*&^%$#@!~";
 
+fn generate_random_word() -> String {
+    let mut rng = thread_rng();
+    let rand_string: Option<String> = (0..rng.gen_range(1, 8))
+        .map(|_| Some(*rng.choose(CHARSET)? as char))
+        .collect();
+
+    match rand_string {
+        Some(s) => s,
+        None => "String not created".to_string(),
+    }
+}
+
+fn generate_sentence() -> String {
+    (0..10)
+        .map(|_| generate_random_word())
+        .collect::<Vec<String>>()
+        .join(" ")
+}
+
+fn main() {
     loop {
-        let mut rng = thread_rng();
-        let rand_string: Option<String> = (0..30)
-            .map(|_| Some(*rng.choose(CHARSET)? as char))
-            .collect();
+        let rand_string = generate_sentence();
 
-        match rand_string {
-            Some(s) => {
-                println!("{}", s);
-            }
-            None => {
-                println!("String was not generated");
-            }
-        }
-
+        println!("{}", rand_string);
         stdout().flush().unwrap();
 
         let now = SystemTime::now();
@@ -32,8 +40,8 @@ fn main() {
 
         match now.elapsed() {
             Ok(elapsed) => {
-                // it prints '2'
-                println!("{}", elapsed.as_secs());
+                let words_per_min = (60.0 / elapsed.as_secs() as f64) * 10.0;
+                println!("{} wpm", words_per_min);
             }
             Err(e) => {
                 // an error occurred!
