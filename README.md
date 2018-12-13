db2af5e883d9618783d804923d7a034fa5bbd510
Rigellute <rigellute@gmail.com>
2018-12-13 09:45:09 +0000

    Add modes, add clap

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 ### Run
 ```sh
 cargo run
+
+# Show cli help options
+cargo run -- --help
 ```
 
 ### TODO
-- [ ] Generate random string with real word length (so we can have spaces)
-- [ ] Calculate words per minute
+- [x] Generate random string with real word length (so we can have spaces)
+- [x] Calculate words per minute
+- [ ] Add cli help options
 - [ ] Calculate accuracy of typing (not sure yet)
+- [x] Add modes (easy, medium, hard)
+- [ ] Add real word option (could find word file)
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,34 @@
+extern crate clap;
 extern crate rand;
+use clap::{App, Arg};
 use rand::prelude::*;
 use std::io::{stdin, stdout, Write};
 use std::time::SystemTime;
 
-const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+const EASY_CHARSET: &[u8] = b"abcdefghijklmnopqrstuvwxyz";
+
+const MEDIUM_CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+const HARD_CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
     abcdefghijklmnopqrstuvwxyz\
     0123456789()[]{}*&^%$#@!~";
 
-fn generate_random_word() -> String {
+enum Mode {
+    Easy,
+    Medium,
+    Hard,
+}
+
+fn generate_random_word(mode: &Mode) -> String {
+    let charset = match mode {
+        Mode::Easy => EASY_CHARSET,
+        Mode::Medium => MEDIUM_CHARSET,
+        Mode::Hard => HARD_CHARSET,
+    };
+
     let mut rng = thread_rng();
     let rand_string: Option<String> = (0..rng.gen_range(1, 8))
-        .map(|_| Some(*rng.choose(CHARSET)? as char))
+        .map(|_| Some(*rng.choose(charset)? as char))
         .collect();
 
     match rand_string {
@@ -19,16 +37,40 @@ fn generate_random_word() -> String {
     }
 }
 
-fn generate_sentence() -> String {
+fn generate_sentence(mode: &Mode) -> String {
     (0..10)
-        .map(|_| generate_random_word())
+        .map(|_| generate_random_word(&mode))
         .collect::<Vec<String>>()
         .join(" ")
 }
 
 fn main() {
+    // Setup basic cli with help options and arg parsing
+    let matches = App::new("TermType")
+        .version("1.0")
+        .author("Alexander Keliris")
+        .about("Practice typing within the terminal 🚀")
+        .arg(
+            Arg::with_name("mode")
+                .help("Specify the level of difficulty: <easy | medium | hard>")
+                .takes_value(true)
+                .short("m")
+                .long("mode"),
+        )
+        .get_matches();
+
+    let mode = match matches.value_of("mode") {
+        Some(m) => match m {
+            "easy" => Mode::Easy,
+            "medium" => Mode::Medium,
+            "hard" => Mode::Hard,
+            _ => Mode::Medium,
+        },
+        None => Mode::Medium,
+    };
+
     loop {
-        let rand_string = generate_sentence();
+        let rand_string = generate_sentence(&mode);
 
         println!("{}", rand_string);
         stdout().flush().unwrap();
@@ -40,7 +82,7 @@ fn main() {
 
         match now.elapsed() {
             Ok(elapsed) => {
-                let words_per_min = (60.0 / elapsed.as_secs() as f64) * 10.0;
+                let words_per_min = ((60.0 / elapsed.as_secs() as f64) * 10.0).floor();
                 println!("{} wpm", words_per_min);
             }
             Err(e) => {
